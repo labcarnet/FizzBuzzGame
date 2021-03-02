726c55a7c62b4f2bc2b0cde07c94a8cc0cfd888d
labcarnet <labcar@labcar.pl>
2021-03-02 01:27:38 +0100

    Add project files.

---
+++ FizzBuzz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+    class FizzBuzz
+    {
+        private string _result;
+        public string SetResult(int userDigit)
+        {
+            if (userDigit % 3 == 0) _result += "Fizz";
+            if (userDigit % 5 == 0) _result += "Buzz";
+            if (userDigit % 3 != 0 && userDigit % 5 != 0) _result = userDigit.ToString();
+            return _result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w grze FizzBuzzGame.\n");
+            var inputDigit = 0;
+            while (true)
+            {
+                Console.WriteLine("Aby zagrać podaj liczbę.\nJeśli chcesz zakończyć grę wciśniej 'Q'.");
+                inputDigit = GetUserDigit();
+
+                var fizzBuzz = new FizzBuzz();
+                Console.WriteLine(fizzBuzz.SetResult(inputDigit) + "\n");
+            }
+        }
+
+        private static int GetUserDigit()
+        {
+            while (true)
+            {
+                var val = Console.ReadLine();
+                if (val.ToUpper() == "Q") Environment.Exit(0);
+
+                if (!int.TryParse(val, out int userDigit))
+                {
+                    Console.WriteLine("\nPrzykro mi. Wprowadzone przez Ciebie dane nie są poprawne. \nPodaj jeszcze raz liczbę całkowitą nie większą jak 4294967295.");
+                    continue;
+                }
+                return userDigit;
+            }
+        }
+    }
+}
